917c82f530119594cb87d538944a281d0fec21fe
Leonard Nolting <leonolting@gmail.com>
2025-03-28 18:11:04 +0100

    Remove old files

--- src/register.rs
+++
@@ -1,9 +0,0 @@
-use rcgen::generate_simple_self_signed;
-
-fn register() {
-    generate_simple_self_signed(vec![
-        "peer_id".to_string(),
-        "localhost".to_string(),
-    ])
-    .unwrap();
-}
