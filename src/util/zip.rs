283215feb6a1b3278d9d7bfdead9ec106d9c74c1
Leonard Nolting <leonolting@gmail.com>
2025-04-08 20:37:42 +0200

    add unzipping and zipping

--- src/util/zip.rs
+++ src/util/zip.rs
@@ -1,3 +1,129 @@
-async fn zip() -> anyhow::Result<()> {
+use std::fs::{create_dir_all, File};
+use std::{fs, io};
+use std::io::prelude::*;
+use std::path::Path;
+
+/// Source: https://github.com/zip-rs/zip2/blob/master/examples/write_dir.rs
+
+use anyhow::Context;
+use walkdir::{DirEntry, WalkDir};
+use zip::{result::ZipError, write::SimpleFileOptions};
+
+fn zip_dir_inner<T>(
+    iterator: &mut dyn Iterator<Item = DirEntry>,
+    prefix: &Path,
+    writer: T,
+    method: zip::CompressionMethod,
+) -> anyhow::Result<()>
+where
+    T: Write + Seek,
+{
+    let mut zip = zip::ZipWriter::new(writer);
+    let options = SimpleFileOptions::default()
+        .compression_method(method)
+        .unix_permissions(0o755);
+
+    let prefix = Path::new(prefix);
+    let mut buffer = Vec::new();
+    for entry in iterator {
+        let path = entry.path();
+        let name = path.strip_prefix(prefix).unwrap();
+        let path_as_string = name
+            .to_str()
+            .map(str::to_owned)
+            .with_context(|| format!("{name:?} Is a Non UTF-8 Path"))?;
+
+        // Write file or directory explicitly
+        // Some unzip tools unzip files with directory paths correctly, some do not!
+        if path.is_file() {
+            println!("adding file {path:?} as {name:?} ...");
+            zip.start_file(path_as_string, options)?;
+            let mut f = File::open(path)?;
+
+            f.read_to_end(&mut buffer)?;
+            zip.write_all(&buffer)?;
+            buffer.clear();
+        } else if !name.as_os_str().is_empty() {
+            // Only if not root! Avoids path spec / warning
+            // and mapname conversion failed error on unzip
+            println!("adding dir {path_as_string:?} as {name:?} ...");
+            zip.add_directory(path_as_string, options)?;
+        }
+    }
+    zip.finish()?;
+    Ok(())
+}
+
+pub fn zip_dir(src_dir: &Path, dst_file: &Path, method: zip::CompressionMethod) -> anyhow::Result<()> {
+    if !Path::new(src_dir).is_dir() {
+        return Err(ZipError::FileNotFound.into());
+    }
+
+    let path = Path::new(dst_file);
+    if let Some(p) = dst_file.parent() {
+        if !p.exists() {
+            create_dir_all(p).unwrap();
+        }
+    }
+    let file = File::create(path).unwrap();
+
+    let walkdir = WalkDir::new(src_dir);
+    let it = walkdir.into_iter();
+
+    zip_dir_inner(&mut it.filter_map(|e| e.ok()), src_dir, file, method)?;
+
+    Ok(())
+}
+
+pub fn unzip_to_dir(archive_path: &Path, directory_path: &Path) -> anyhow::Result<()> {
+    let file = File::open(archive_path).unwrap();
+
+    let mut archive = zip::ZipArchive::new(file).unwrap();
+
+    for i in 0..archive.len() {
+        let mut file = archive.by_index(i).unwrap();
+        let outpath = match file.enclosed_name() {
+            Some(path) => path,
+            None => continue,
+        };
+        let outpath = Path::new(directory_path).join(outpath);
+
+        {
+            let comment = file.comment();
+            if !comment.is_empty() {
+                println!("File {i} comment: {comment}");
+            }
+        }
+
+        if file.is_dir() {
+            println!("File {} extracted to \"{}\"", i, outpath.display());
+            create_dir_all(&outpath).unwrap();
+        } else {
+            println!(
+                "File {} extracted to \"{}\" ({} bytes)",
+                i,
+                outpath.display(),
+                file.size()
+            );
+            if let Some(p) = outpath.parent() {
+                if !p.exists() {
+                    create_dir_all(p).unwrap();
+                }
+            }
+            let mut outfile = File::create(&outpath).unwrap();
+            io::copy(&mut file, &mut outfile).unwrap();
+        }
+
+        // Get and Set permissions
+        #[cfg(unix)]
+        {
+            use std::os::unix::fs::PermissionsExt;
+
+            if let Some(mode) = file.unix_mode() {
+                fs::set_permissions(&outpath, fs::Permissions::from_mode(mode)).unwrap();
+            }
+        }
+    }
+    
     Ok(())
 }
