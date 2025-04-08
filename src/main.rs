0af7b14b0db9d3a991fb1b5f33a8d03ce13b5732
Leonard Nolting <leonolting@gmail.com>
2025-04-08 20:34:45 +0200

    add high level functions

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
+use std::sync::Arc;
 use crate::cli::cli;
 use crate::util::logging::start_logger;
 use anyhow::{Result};
 use util::response_manager::ResponseManager;
 use webrtc::peer_connection::certificate::RTCCertificate;
+use crate::core::p2p::peer::PeerId;
+use crate::core::p2p::session::Session;
+use crate::core::proxies::server::jude_server;
+use crate::infra::storage::{get_server, run_server};
 
 mod util;
 mod cli;
@@ -15,6 +20,23 @@ async fn main() -> Result<()> {
     start_logger()?;
 
     cli().await?;
+    
+    Ok(())
+}
+
+pub async fn share_running_server(name: PeerId, session: Arc<Session>, address: &str) -> Result<()> {
+    jude_server(name, session, address).await?;
+    
+    Ok(())
+}
+
+pub async fn download_and_run_server(name: PeerId, session: Arc<Session>, address: &str) -> Result<()> {
+    get_server(name.clone()).await?;
+    
+    run_server(name.clone()).await?;
+    
+    // TODO close server whenever something crashes?!
+    jude_server(name, session, address).await?;
     
     Ok(())
 }
