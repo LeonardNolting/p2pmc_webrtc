d2e5cbc1b76d2546218d5f18e009241d8bcd8f77
Leonard Nolting <leonolting@gmail.com>
2025-04-12 14:34:01 +0200

    test_server

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,16 @@ pub use crate::core::p2p::session as session;
 pub mod util;
 pub mod core;
 pub mod crypto;
+
+#[cfg(test)]
+mod tests {
+    use crate::core::proxies::server::jude_server;
+    use crate::session::Session;
+    use super::*;
+
+    #[tokio::test]
+    async fn test_server() {
+        let session = Session::new("ws://34.75.203.169:5100".to_owned()).await.unwrap();
+        jude_server("hyperpixel".to_owned(), &session, "127.0.0.1:3000").await.unwrap();
+    }
+}
