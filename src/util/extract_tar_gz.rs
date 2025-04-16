b2f5c9329c16e4ec32250cb0773228b23c7bf515
Leonard Nolting <leonolting@gmail.com>
2025-04-16 19:19:08 +0200

    add todo

--- src/util/extract_tar_gz.rs
+++ src/util/extract_tar_gz.rs
@@ -2,6 +2,7 @@ use flate2::read::GzDecoder;
 use tar::Archive;
 
 // TODO convert data to a Stream of bytes, to extract while downloading
+// TODO allow cancellation
 pub fn extract_tar_gz(data: &[u8], extraction_path: &str) -> Result<(), std::io::Error> {
     let tar = GzDecoder::new(data);
     let mut archive = Archive::new(tar);
