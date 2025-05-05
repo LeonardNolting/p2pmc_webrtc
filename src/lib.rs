9e46352608975779e6f32be6f4bf31c3920f76af
Leonard Nolting <leonolting@gmail.com>
2025-05-05 12:48:08 +0200

    add old crypto code as test

--- src/lib.rs
+++ src/lib.rs
@@ -50,6 +50,29 @@ mod tests {
     use std::thread;
     use std::time::Duration;
     use crate::util::logging::start_logger;
+    use crate::util::crypto;
+    
+    #[tokio::test]
+    async fn old_crypto_code() {
+        // let (root_certified_key, root_age_key) = crypto::create_root().await.unwrap();
+        let root_certified_key = crypto::load_root().await.unwrap();
+
+        let is_client = std::env::args().nth(1).expect("Server or client?") == "client";
+        let id = std::env::args().nth(2).expect("Provide an ID");
+
+        let (user_certified_key, user_age_key) = crypto::create_user(id.clone(), &root_certified_key).await.unwrap();
+        let user_rtc_cert = crypto::load_user(id.clone()).await.unwrap();
+
+        // crypto::parse_cert(user_certified_key.cert.der()).await.unwrap();
+
+        if is_client {
+            let port = std::env::args().nth(3).unwrap_or("25565".to_string()).parse::<u16>().expect("Port must be a number");
+            // start_client_proxy(SIGNALING_SERVER, id.as_str(), port, user_rtc_cert, root_certified_key.cert.der().to_vec()).await;
+        } else {
+            let port = std::env::args().nth(3).expect("Provide a port on which a Minecraft server runs").parse::<u16>().expect("Port must be a number");
+            // start_server_proxy(SIGNALING_SERVER, id.as_str(), port, user_rtc_cert).await;
+        }
+    }
 
     #[tokio::test]
     async fn test_logger() {
